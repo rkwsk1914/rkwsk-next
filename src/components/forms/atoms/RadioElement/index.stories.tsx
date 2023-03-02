de2a90c04e21f28c51412c88857007a7291a4236
Ryo Kawasaki <ryokawasaki@RyonoMBP.lan>
2023-03-03 02:49:37 +0900

    add: RadioElement

---
+++ src/components/forms/atoms/RadioElement/index.stories.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+
+import BookmarkIcon from '@mui/icons-material/Bookmark'
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder'
+
+import { action } from "@storybook/addon-actions";
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import { InputAttributeOptions } from '@/types/InputAttribute'
+
+import { RadioElement as StoryComponent } from '.'
+
+export default {
+  title: 'Forms/Atoms/RadioElement',
+  component: StoryComponent,
+  argTypes: {
+    disabled: InputAttributeOptions.disabled,
+    error: InputAttributeOptions.error,
+    required: InputAttributeOptions.required,
+  }
+} as ComponentMeta<typeof StoryComponent>
+
+const Template: ComponentStory<typeof StoryComponent> = (
+  args: React.ComponentProps<typeof StoryComponent>
+) => (
+  <StoryComponent {...args}></StoryComponent>
+)
+
+export const Default = Template.bind({})
+Default.args = {
+  checked: false,
+  size: "small",
+  disabled: false,
+  label: 'default',
+  onChange: action('onChange'),
+  value: ""
+}
+
+export const Uncontrolled = Template.bind({})
+Uncontrolled.args = {
+  defaultChecked: false,
+  size: "small",
+  disabled: false,
+  label: 'default',
+  onChange: action('onChange'),
+  value: ""
+}
+
+export const Bookmark = Template.bind({})
+Bookmark.args = {
+  defaultChecked: false,
+  size: "small",
+  disabled: false,
+  onChange: action('onChange'),
+  value: "",
+  icon: <BookmarkBorderIcon />,
+  checkedIcon: <BookmarkIcon />
+}
