df3a08747960c787207e3739bff0a3f61a151633
Ryo Kawasaki <ryokawasaki@RyonoMBP.lan>
2023-04-17 21:56:20 +0900

    fix: move storybook v7

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,29 +1,22 @@
 const path = require('path');
+
 module.exports = {
-  "stories": ["../src/components/**/*.stories.mdx", "../src/components/**/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-actions", "@storybook/addon-mdx-gfm"],
-  "framework": {
+  stories: ["../src/components/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-actions",
+    "@storybook/addon-docs"
+  ],
+  framework: {
     name: "@storybook/nextjs",
     options: {}
   },
-  "core": {
-    "builder": "@storybook/builder-webpack5"
+  core: {
+    builder: "@storybook/builder-webpack5"
   },
   webpackFinal: async config => {
-    // scss を読み込む
-    config.module.rules.push({
-      test: /\.scss$/,
-      use: ['style-loader', {
-        loader: 'css-loader',
-        options: {
-          importLoaders: 1,
-          // 1 => postcss-loader
-          modules: {
-            localIdentName: '[local]___[hash:base64:2]'
-          }
-        }
-      }, 'sass-loader']
-    });
     config.resolve.alias = {
       ...config.resolve.alias,
       '@': path.resolve(__dirname, '../src')
