399d52e6af37f8704f0e7da58c1fa5803b4ccb06
Ryo Kawasaki <ryokawasaki@RyonoMBP.lan>
2023-04-17 21:10:47 +0900

    install mui

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,33 +1,46 @@
 const path = require('path');
 module.exports = {
   "stories": ["../src/components/**/*.stories.mdx", "../src/components/**/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-actions", "@storybook/addon-mdx-gfm"],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-actions",
+    "@storybook/addon-mdx-gfm"
+  ],
   "framework": {
     name: "@storybook/nextjs",
     options: {}
   },
-  webpackFinal: async config => {
+  "core": {
+    "builder": "@storybook/builder-webpack5"
+  },
+  webpackFinal: async (config) => {
     // scss を読み込む
+    /*
     config.module.rules.push({
       test: /\.scss$/,
-      use: ['style-loader', {
-        loader: 'css-loader',
-        options: {
-          importLoaders: 1,
-          // 1 => postcss-loader
-          modules: {
-            localIdentName: '[local]___[hash:base64:2]'
-          }
-        }
-      }, 'sass-loader']
-    });
+      use: [
+        'style-loader',
+        {
+          loader: 'css-loader',
+          options: {
+            importLoaders: 1, // 1 => postcss-loader
+            modules: {
+              localIdentName: '[local]___[hash:base64:2]',
+            },
+          },
+        },
+        'sass-loader',
+      ],
+    })
+    */
+
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@': path.resolve(__dirname, '../src')
+      '@': path.resolve(__dirname, '../src'),
     };
-    return {
-      ...config
-    };
+    return {...config};
   },
   docs: {
     autodocs: true
