db047a4cf06403d27763e7fe70b24a7e188f1676
Ryo Kawasaki <ryokawasaki@RyonoMBP.lan>
2023-04-27 20:54:32 +0900

    fix: set original color to DarkThema component.

--- src/const/DarkTheme.ts
+++ src/const/DarkTheme.ts
@@ -1,14 +1,22 @@
 import { createTheme } from '@mui/material/styles'
 
+import { colors } from './../../tailwind/colors'
+
 
 export const darkTheme = createTheme({
   palette: {
     mode: 'dark',
+    background: {
+      default: colors.darkBg
+    }
   },
 })
 
 export const lightTheme = createTheme({
   palette: {
     mode: 'light',
+    background: {
+      default: colors.lightBg
+    }
   },
 })
