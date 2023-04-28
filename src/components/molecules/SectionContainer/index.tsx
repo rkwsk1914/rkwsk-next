11ac5e6f9854b9cbc8f4755671005204db2a3c31
Ryo Kawasaki <ryokawasaki@RyonoMBP.lan>
2023-04-28 14:47:09 +0900

    new: SectionContainer.

---
+++ src/components/molecules/SectionContainer/index.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react'
+
+import clsx from 'clsx'
+import { SectionTitle } from '@/components/atoms/SectionTitle'
+import { useGetDarkModeStyleClass } from '@/hooks/useGetDarkModeStyleClass'
+
+import styles from './style.module.scss'
+
+type Props = {
+  children?: React.ReactNode
+  title?: React.ReactNode
+  level?: 1 | 2 | 3 | 4 | 5 | 6
+};
+
+export const SectionContainer: React.FC<Props> = (
+  {
+    children,
+    title,
+    level = 2
+  }
+): JSX.Element => {
+  const className = clsx(styles.content, {
+    [styles.level1]: level === 1,
+    [styles.level2]: level === 2,
+    [styles.level3]: level === 3,
+    [styles.level4]: level === 4,
+    [styles.level5]: level === 5,
+    [styles.level6]: level === 6,
+  })
+
+  return (
+    <section className={styles.section}>
+      <SectionTitle level={level}>{title}</SectionTitle>
+      <div className={className}>
+        {children}
+      </div>
+    </section>
+  )
+}
