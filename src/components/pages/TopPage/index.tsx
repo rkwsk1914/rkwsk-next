8c3f017ff476cd7268e71bf104323ef2766eb54a
Ryo Kawasaki <ryokawasaki@RyonoMBP.lan>
2023-05-16 06:04:23 +0900

    add: Logo into TopPage.

--- src/components/pages/TopPage/index.tsx
+++ src/components/pages/TopPage/index.tsx
@@ -1,16 +1,20 @@
 import * as React from 'react'
 
 import { FirstViewBackGround } from '@/components/atoms/FirstViewBackGround'
+import { Logo } from '@/components/atoms/Logo'
 import { SectionContainer } from '@/components/molecules/SectionContainer'
 import { MySite } from '@/components/templates/MySite'
 
-//import styles from './style.module.scss'
+import styles from './style.module.scss'
 
 type Props = {};
 
 export const TopPage: React.FC<Props> = (): JSX.Element => {
   return (
     <MySite title={'rkwsk'} description={'rkwsk'}>
+      <SectionContainer>
+        <div className={styles.logo}><Logo /></div>
+      </SectionContainer>
       <FirstViewBackGround imgSrc='/cafe.jpg'>
         <SectionContainer title={'Hello World'} level={1}>
           Hello
