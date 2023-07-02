22faf686741a7e31801b4a509d51de1cdc58994b
Ryo Kawasaki <ryokawasaki@RyonoMacBook-Pro.local>
2023-07-02 17:04:20 +0900

    add: plx.

---
+++ src/components/sample/PlxTest/index.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import { StoryFn, Meta } from '@storybook/react'
+
+import { PlxTest as StoryComponent } from '.'
+
+export default {
+  title: 'Sample/PlxTest',
+  component: StoryComponent,
+} as Meta<typeof StoryComponent>
+
+const Template: StoryFn<typeof StoryComponent> = (
+  args?: React.ComponentProps<typeof StoryComponent>
+) => (
+  <StoryComponent {...args}></StoryComponent>
+)
+
+export const Default = Template.bind({})
+Default.args = {
+  children: 'Sample',
+}
