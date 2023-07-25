78c195bf91f921675784b507262065d01ed2a066
Ryo Kawasaki <ryokawasaki@RyonoMBP.lan>
2023-07-25 22:19:39 +0900

    fix: MySkillSetSection use SlickSlider.

--- src/components/pages/TopPage/MySkillSetSection/index.tsx
+++ src/components/pages/TopPage/MySkillSetSection/index.tsx
@@ -1,48 +1,45 @@
 import * as React from 'react'
 
-import Slider from "react-slick"
-import { Settings } from "react-slick"
-import "slick-carousel/slick/slick.css"
-import "slick-carousel/slick/slick-theme.css"
-
 import { useMatchHeight } from '@/hooks/useMatchHeight'
 
 import { SKILL_SET_DATA } from '@/const/page/SkillSetData'
 
-import { Card } from '@/components/atoms/Card'
+import { MyCard } from '@/components/atoms/MyCard'
+import { SlickSlider, OriginalSettings } from '@/components/atoms/SlickSlider'
 import { SectionContainer } from '@/components/molecules/SectionContainer'
 import { SkillSetTable } from '@/components/organisms/SkillSetTable'
 
-type Props = {};
+import styles from './style.module.scss'
 
-export const MySkillSetSection: React.FC<Props> = ({}): JSX.Element => {
+export const MySkillSetSection: React.FC = (): JSX.Element => {
   const dataLength = SKILL_SET_DATA.length
   const {height, refsArray} = useMatchHeight(dataLength)
 
   const slides = SKILL_SET_DATA.map((item, index) => (
-    <Card key={index} title={item.title} ref={refsArray.current[index]} matchHeight={height}>
-      <SkillSetTable data={item.data} />
-    </Card>
+    <div key={index} className={styles.cardWrap}>
+      <MyCard title={item.title} ref={refsArray.current[index]} matchHeight={height}>
+        <SkillSetTable data={item.data} />
+      </MyCard>
+    </div>
   ))
 
-  const settings: Settings = {
-    infinite:true, //to allow the slides to show infinitely
-    speed: 300, //this is the speed of slider in ms
-    slidesToShow:1, //number of slides to show up on screen
-    centerPadding: '50px',
+  const settings: OriginalSettings = {
+    infinite:true,
+    speed: 300,
+    slidesToShow:1,
+    centerPadding: '150px',
     autoplay: true,
     arrows: true,
     dots: true,
-    centerMode: true
+    centerMode: true,
+    useCSS: true
   }
 
   return (
     <SectionContainer id={'skills'} title={'Skills'}>
-      <div>
-        <Slider {...settings}>
-          {slides}
-        </Slider>
-      </div>
+      <SlickSlider settings={settings}>
+        {slides}
+      </SlickSlider>
     </SectionContainer>
   )
 }
