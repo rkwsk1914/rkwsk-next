0bcfbe47e9ff3f7b0ed593d02fbed8d5bec6e4c1
Ryo Kawasaki <ryokawasaki@RyonoMBP.lan>
2023-07-25 22:18:41 +0900

    create: SlickSlider.

---
+++ src/components/atoms/SlickSlider/index.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import { StoryFn, Meta } from '@storybook/react'
+
+import { SlickSlider as StoryComponent } from '.'
+
+export default {
+  title: 'Atoms/SlickSlider',
+  component: StoryComponent,
+} as Meta<typeof StoryComponent>
+
+const Template: StoryFn<typeof StoryComponent> = (
+  args: React.ComponentProps<typeof StoryComponent>
+) => (
+  <StoryComponent {...args}></StoryComponent>
+)
+
+export const Default = Template.bind({})
+Default.args = {}
