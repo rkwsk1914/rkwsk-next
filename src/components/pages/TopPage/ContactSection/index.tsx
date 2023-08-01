0dc66354a197ed82d8502621e9b01ded05b66c5e
Ryo Kawasaki <ryokawasaki@RyonoMBP.lan>
2023-08-01 13:21:48 +0900

    create: ContactSection

---
+++ src/components/pages/TopPage/ContactSection/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+
+import { SectionContainer } from '@/components/molecules/SectionContainer'
+
+// import styles from './style.module.scss'
+
+type Props = {}
+
+export const ContactSection: React.FC<Props> = ({}): JSX.Element => {
+  return (
+    <SectionContainer id={'contact'} title={'Contact'}>
+
+    </SectionContainer>
+  )
+}
