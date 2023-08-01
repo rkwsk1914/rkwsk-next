e7a18774806e75dbc47dd3fc7397fa6ab4c7d9b5
Ryo Kawasaki <ryokawasaki@RyonoMBP.lan>
2023-08-01 16:55:37 +0900

    add: ContactForm in ContactSection.

--- src/components/pages/TopPage/ContactSection/index.tsx
+++ src/components/pages/TopPage/ContactSection/index.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react'
 
+import { ContactForm } from '@/components/forms/templates/ContactForm'
 import { SectionContainer } from '@/components/molecules/SectionContainer'
+
 
 // import styles from './style.module.scss'
 
@@ -9,7 +11,7 @@ type Props = {}
 export const ContactSection: React.FC<Props> = ({}): JSX.Element => {
   return (
     <SectionContainer id={'contact'} title={'Contact'}>
-
+      <ContactForm onSubmit={() => console.log('submit')}/>
     </SectionContainer>
   )
 }
