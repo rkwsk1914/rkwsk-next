a5aa075396e0d41c2cfe7239d729049f2336830b
Ryo Kawasaki <ryokawasaki@RyonoMBP.lan>
2023-08-22 03:21:26 +0900

    fix: style LiquidShapeImage

--- src/components/atoms/LiquidShapeImage/index.stories.tsx
+++ src/components/atoms/LiquidShapeImage/index.stories.tsx
@@ -20,6 +20,7 @@ Default.args = {
   image: {
     src: '/pic-me.jpg',
     width: 1478,
-    height: 1108
+    height: 1108,
+    alt: 'test'
   },
 }
--- src/components/atoms/LiquidShapeImage/index.tsx
+++ src/components/atoms/LiquidShapeImage/index.tsx
@@ -7,20 +7,20 @@ import { useGetDarkModeStyleClass } from '@/hooks/useGetDarkModeStyleClass'
 import styles from './style.module.scss'
 
 type Props = {
-  image: Omit<React.ComponentProps<typeof Image>, 'alt' | 'className'>
+  image: Omit<React.ComponentProps<typeof Image>, 'className'>
 };
 
 export const LiquidShapeImage: React.FC<Props> = (
   {
-    image
+    image,
   }
 ): JSX.Element => {
-  const className = useGetDarkModeStyleClass(styles.li, styles.dark)
+  const classNameContainer = useGetDarkModeStyleClass(styles.container, styles.dark)
   return (
-    <div className={styles.container}>
-      <div className={styles.fluid}></div>
-      <div className={styles.fluid2}></div>
-      <Image className={styles.image} alt={``} {...image} />
+    <div className={classNameContainer}>
+      <div className={styles.fluid} />
+      <div className={styles.fluid2} />
+      <Image className={styles.image} {...image} />
     </div>
   )
 }
