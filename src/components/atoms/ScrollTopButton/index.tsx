1491a3fec3ebad92867ff448e24ef7ef89278288
Ryo Kawasaki <ryokawasaki@RyonoMBP.lan>
2023-08-22 17:14:52 +0900

    fix: ScrollTopButton.

--- src/components/atoms/ScrollTopButton/index.tsx
+++ src/components/atoms/ScrollTopButton/index.tsx
@@ -1,21 +1,22 @@
 import * as React from 'react'
 
+import { animateScroll } from 'react-scroll'
+
 import { useGetDarkModeStyleClass } from '@/hooks/useGetDarkModeStyleClass'
 
 import { ICON_DATA } from '@/const/IconData'
 
 import styles from './style.module.scss'
 
-type Props = {
-  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void
-};
-
-export const ScrollTopButton: React.FC<Props> = (
-  {
-    onClick
-  }
-): JSX.Element => {
+export const ScrollTopButton: React.FC = (): JSX.Element => {
   const className = useGetDarkModeStyleClass(styles.btn, styles.dark)
+
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    animateScroll.scrollToTop({
+      smooth: true
+    })
+  }
+
   return (
     <button className={className} onClick={onClick}>
       {ICON_DATA.circleChevronUp}
