c90ac38132be3f349dbd95cdc1df688e155ddb70
Ryo Kawasaki <ryokawasaki@RyonoMBP.lan>
2023-08-24 06:50:51 +0900

    remove: global nav work link.

--- src/const/page/GlobalNavData.ts
+++ src/const/page/GlobalNavData.ts
@@ -4,7 +4,7 @@ export const GLOBAL_NAV_DATA: GlobalNavMenuType = {
   profile: { text: 'Profile', id: 'profile' },
   skills: { text: 'Skills', id: 'skills' },
   history: { text: 'History', id: 'history' },
-  works: { text: 'Works', id: 'works' },
+  // works: { text: 'Works', id: 'works' },
   contact: { text: 'Contact', id: 'contact' },
 }
 
