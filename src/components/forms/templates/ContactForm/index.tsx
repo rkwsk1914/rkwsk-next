9878a3f669dd340e39d81adc5fe94c5375158317
Ryo Kawasaki <ryokawasaki@RyonoMBP.lan>
2023-08-24 17:06:32 +0900

    fix: Organize duplicate id.

--- src/components/forms/templates/ContactForm/index.tsx
+++ src/components/forms/templates/ContactForm/index.tsx
@@ -19,7 +19,7 @@ type Inputs = {
   lastKanaName: string,
   tel: string,
   email: string,
-  contact: string,
+  content: string,
 };
 
 type Props = {
@@ -36,7 +36,7 @@ export const ContactForm: React.FC<Props> = ({
     'lastKanaName',
     'tel',
     'email',
-    'contact',
+    'content',
   ]
   const { register, trigger, handleSubmit, setValue, formState: { errors, isDirty, isValid } } = useForm<Inputs>({
     resolver: zodResolver(SCHEMA),
