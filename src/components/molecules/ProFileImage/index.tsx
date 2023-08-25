a2f0922d0b11727fea354f30c8ad2759551fc4bf
Ryo Kawasaki <ryokawasaki@RyonoMBP.lan>
2023-08-26 04:12:22 +0900

    fix: ProFileImage.

---
+++ src/components/molecules/ProFileImage/index.tsx
@@ -0,0 +1,53 @@
+import * as React from 'react'
+
+import Image from 'next/image'
+
+import { useGetDarkModeStyleClass } from '@/hooks/useGetDarkModeStyleClass'
+
+import { LiquidShapeImage } from '@/components/atoms/LiquidShapeImage'
+
+
+import styles from './style.module.scss'
+
+type Props = {
+  image: Omit<React.ComponentProps<typeof Image>, 'alt' | 'className'>
+  sectionLevel?: 2 | 3 | 4 | 5 | 6,
+  children?: React.ReactNode
+}
+
+export const ProFileImage: React.FC<Props> = ({
+  image,
+  sectionLevel = 2,
+  children,
+}): JSX.Element => {
+  const itemClassName = useGetDarkModeStyleClass(styles.item, styles.dark)
+  const myName = (<>{'KAWASAKI'}<br />{'RYO'}</>)
+
+  const headingClassName = styles.headingText
+
+  const HeadingTag = () => {
+    switch (sectionLevel) {
+      case 3: return <h3 className={headingClassName}>{myName}</h3>
+      case 4: return <h4 className={headingClassName}>{myName}</h4>
+      case 5: return <h5 className={headingClassName}>{myName}</h5>
+      case 6: return <h6 className={headingClassName}>{myName}</h6>
+      default: return <h2 className={headingClassName}>{myName}</h2>
+    }
+  }
+
+  return (
+    <div className={itemClassName}>
+      <div className={styles.main_content}>
+        <HeadingTag />
+        <div className={styles.image_wrap}>
+          <LiquidShapeImage
+            alt='profile'
+            image={image} />
+        </div>
+        <span className={styles.backText}>{myName}</span>
+        <small className={styles.subText}>Front End Engineer</small>
+      </div>
+      {children && <div className={styles.content}>{children}</div>}
+    </div>
+  )
+}
