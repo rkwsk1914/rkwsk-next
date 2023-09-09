52c35a4a3446963e2600be9f86fad74e654dbafc
Ryo Kawasaki <ryokawasaki@RyonoMacBook-Pro.local>
2023-09-09 15:00:54 +0900

    add: top button in ErrorPage.

--- src/components/pages/ErrorPage/index.tsx
+++ src/components/pages/ErrorPage/index.tsx
@@ -2,8 +2,11 @@ import * as React from 'react'
 
 import { ErrorPageData } from '@/const/ErrorPageData'
 
+import { Button } from '@/components/atoms/Button'
 import { SectionContainer } from '@/components/molecules/SectionContainer'
 import { MySite } from '@/components/templates/MySite'
+
+import styles from './style.module.scss'
 
 type Props = {
   errorId: keyof typeof ErrorPageData
@@ -14,7 +17,11 @@ export const ErrorPage: React.FC<Props> = ({
 }): JSX.Element => {
   return (
     <MySite title={ErrorPageData[errorId].title} description={ErrorPageData[errorId].description}>
-      <SectionContainer level={1} title={ErrorPageData[errorId].content} isFull />
+      <SectionContainer level={1} title={ErrorPageData[errorId].content} isFull>
+        <div className={styles.button_wrap} >
+          <Button href='/' type='outline' >トップページに戻る</Button>
+        </div>
+      </SectionContainer>
     </MySite>
   )
 }
