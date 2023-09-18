8e2cd7f36114901872b172cf624010648a6a57fd
Ryo Kawasaki <ryokawasaki@RyonoMacBook-Pro.local>
2023-09-18 21:30:47 +0900

    fix: footer and ErrorPage page height.

--- src/components/layouts/MyFooter/index.tsx
+++ src/components/layouts/MyFooter/index.tsx
@@ -1,8 +1,18 @@
 import * as React from 'react'
 
+import { useRouter } from "next/router"
+
 import { useGetDarkModeStyleClass } from '@/hooks/useGetDarkModeStyleClass'
 
+import { GLOBAL_NAV_DATA, GLOBAL_TOP_NAV_DATA } from '@/const/page/GlobalNavData'
+
+import { MenuList } from '@/components/molecules/MenuList'
+
+
+
 import styles from './style.module.scss'
+
+import { MenuDataType } from '@/types/MenuDataType'
 
 type Props = {
   children?: React.ReactNode
@@ -14,7 +24,18 @@ export const MyFooter: React.FC<Props> = (
   }
 ): JSX.Element => {
   const footerClassName = useGetDarkModeStyleClass(styles.footer, styles.dark)
+  const router = useRouter()
+
+  const getGlobalNavDataArray = (): MenuDataType[] => {
+    const data = (router.pathname === '/') ? GLOBAL_TOP_NAV_DATA : GLOBAL_NAV_DATA
+    const keys = Object.keys(data)
+
+    return keys.map(key => data[key])
+  }
+
   return (
-    <footer className={footerClassName}></footer>
+    <footer className={footerClassName}>
+      <MenuList data={getGlobalNavDataArray()} type='horizontality' />
+    </footer>
   )
 }
