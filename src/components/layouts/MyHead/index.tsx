1c55d725ccf4de634f7efb34c4bc1137b3482113
Ryo Kawasaki <ryokawasaki@RyonoMacBook-Pro.local>
2023-09-18 21:36:28 +0900

    add: favicon icon.

--- src/components/layouts/MyHead/index.tsx
+++ src/components/layouts/MyHead/index.tsx
@@ -18,7 +18,7 @@ export const MyHead: React.FC<Props> = (
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/logo.ico" />
     </Head>
   )
 }
